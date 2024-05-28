9d346f84f3b19aa733ccbf2bf3021d2ff5fedb08
gamze <gamzekandara@gmail.com>
2024-05-28 18:13:50 -0400

    insert.sql

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'F', 'white', 'calm', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'F', 'white', 'calm', TRUE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'white', 'calm', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'white', 'calm', TRUE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'white', 'calm', FALSE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'white', 'calm', FALSE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'white', 'calm', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'white', 'calm', TRUE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'white', 'calm', FALSE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'white', 'calm', FALSE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'F', 'white', 'calm', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'F', 'white', 'calm', TRUE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'white', 'calm', fALSE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'white', 'calm', fALSE);
 
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'white', 'calm', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'white', 'calm', TRUE);
